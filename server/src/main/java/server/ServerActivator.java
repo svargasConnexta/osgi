dc06a1e4ff8f2721618bea27d558b4055dad09bd
Samuel Vargas <samuel.vargas@connexta.com>
2018-06-11 17:25:14 -0700

    Revert later

--- server/src/main/java/server/ServerActivator.java
+++ server/src/main/java/server/ServerActivator.java
@@ -2,26 +2,38 @@ package server;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
 
 import java.io.IOException;
 
-public class ServerActivator implements BundleActivator {
+public class ServerActivator implements BundleActivator, BundleListener {
 
   private ClientConnectionThreadSpawner clientConnectionThreadSpawner;
   private Thread connectionListenThread;
+  //private PluginListener pluginListener;
   private final int maxUsers = 10;
-  private final int maxMessages = 9999;
   private final int port = 8080;
 
+  @Override
   public void start(BundleContext bundleContext) throws IOException {
     MessageDispatcher messageDispatcher = new DefaultMessageDispatcher(maxUsers);
     clientConnectionThreadSpawner = new ClientConnectionThreadSpawner(messageDispatcher, port);
     connectionListenThread = new Thread(clientConnectionThreadSpawner);
     connectionListenThread.start();
+    //pluginListener = new PluginListener();
+    //bundleContext.addBundleListener(pluginListener);
+    //bundleContext.getServiceReferences()
   }
 
+  @Override
   public void stop(BundleContext bundleContext) throws Exception {
     clientConnectionThreadSpawner.exit();
     connectionListenThread.join();
+    //bundleContext.removeBundleListener(pluginListener);
+  }
+
+  @Override
+  public void bundleChanged(BundleEvent event) {
   }
 }
---
+++ serverplugin/src/main/java/serverplugin/ConversionPlugin.java
@@ -0,0 +1,5 @@
+package serverplugin;
+
+public interface ConversionPlugin {
+    public String convertMessage(String message) ;
+}
